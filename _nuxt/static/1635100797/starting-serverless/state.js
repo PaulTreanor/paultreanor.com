d0e975d00052b34fd8388e8704efd2e6b9415a74
PaulTreanor <PaulTreanor@users.noreply.github.com>
2021-11-07 11:54:36 +0000

    deploy: 0b62a6732342cc68c6d05d978056a595db516da2

--- _nuxt/static/1635100797/starting-serverless/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635100797",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstarting-serverless",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4d4a1c19"}}}}(null));
