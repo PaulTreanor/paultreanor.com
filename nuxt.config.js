dc8d90ceffafcc25064457767d70f149632dc171
paultreanor <paul.treanor@fourtheorem.com>
2022-08-20 13:21:32 +0100

    Change favicon to mushroom emoji

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,14 @@ export default {
       { hid: 'description', name: 'description', content: 'My website where I talk about software development :)' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-      { rel: 'stylesheet', type: "text/css", href: '/css2.css' },
-      { rel: 'stylesheet', type: "text/css", href: '/skeleton-main.css' }    ],
+      { rel: 'icon', type: 'image/x-icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size="100">🍄</text></svg>' },
+      { rel: 'stylesheet', type: 'text/css', href: '/css2.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/skeleton-main.css' }],
     script: [
-      {        src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js",      },
-      { src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js" }
-    
-    ],
+      { src: 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js' },
+      { src: 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js' }
+
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -47,7 +47,7 @@ export default {
     '@nuxtjs/axios',
     'nuxt-content-body-html',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content', 
+    '@nuxt/content',
     '@nuxtjs/feed'
   ],
   bootstrapVue: {
@@ -61,27 +61,26 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }, 
+  },
 
   feed: [
     // A default feed configuration object
     {
       path: '/rss.xml', // The route to your feed.
-      create: async feed => {
-        
+      create: async (feed) => {
         const $content = require('@nuxt/content').$content
 
         feed.options = {
           title: "Paul Treanor's blog",
           link: 'https://paultreanor.com/blog',
-          description: "Bloggo :)",
+          description: 'Bloggo :)'
         }
 
-          const posts = await $content('blog')
+        const posts = await $content('blog')
           .sortBy('createdAt', 'desc')
           .fetch()
 
-            posts.forEach(post => {
+        posts.forEach((post) => {
           const url = `https://paultreanor.com/blog/${post.slug}`
           feed.addItem({
             author: post.authors,
@@ -90,13 +89,12 @@ export default {
             description: post.description,
             id: url,
             link: url,
-            title: post.title,
+            title: post.title
           })
         })
-
       },
       cacheTime: 1000 * 60 * 15, // How long should the feed be cached
-      type: 'rss2', // Can be: rss2, atom1, json1
+      type: 'rss2' // Can be: rss2, atom1, json1
     }
   ]
 }
