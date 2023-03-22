171777199fe1ddcf5e5c1a6777338dd989288b34
paultreanor <paul.treanor@fourtheorem.com>
2023-03-22 06:19:49 +0000

    Add vercel next.js demo blog code
    - https://nextjs.org/learn/basics/data-fetching/setup

---
+++ pages/posts/[id].js
@@ -0,0 +1,45 @@
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+import Head from 'next/head'
+
+
+
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
