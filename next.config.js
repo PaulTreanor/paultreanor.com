85e3df11a6bb722e0e1b457a2fa33e7b579da34b
paultreanor <paul.treanor@fourtheorem.com>
2023-03-24 23:28:20 +0000

    Configure next to use SSG
    - Change npm run build to build and export
    - Change gh-pages directory to be out
    - Configure next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  output: {
+    // Export in the format expected by GitHub Pages
+    // or any static site hosting service
+    publicDirectory: "/",
+  },
+  images: {
+    unoptimized: true,
+  },
+};
