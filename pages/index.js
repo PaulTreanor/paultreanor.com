36fc116dd0b7b6bc817fb3ff3a4bb93a6400c2da
paultreanor <paul.treanor@fourtheorem.com>
2023-07-17 21:24:57 +0100

    Add pop of colour to home page :)

--- pages/index.js
+++ pages/index.js
@@ -77,22 +77,22 @@ export default function Home({ allPagesData }) {
           <h2 className="text-4xl font-open-sans font-bold py-4">📝 Latest Notes</h2>
           <div className="articles pb-40">
             <div className="search-box py-2">
-              <input value={search} onChange={(event) => setSearch(event.target.value)} type="text" placeholder="Search for tags or post titles 🔎" className="bg-slate-50 border border-sky-300 text-slate-900 rounded-lg active:border-sky-400 hover:border-sky-400 focus:border-sky-400 block p-2.5 w-96 max-w-full"/>
+              <input value={search} onChange={(event) => setSearch(event.target.value)} type="text" placeholder="Search for tags or post titles 🔎" className="bg-slate-50 border border-sky-300 text-slate-900 rounded-lg active:border-sky-400 active:bg-yellow-50 focus:bg-yellow-50 hover:border-sky-400 focus:border-sky-400 block p-2.5 w-96 max-w-full"/>
             </div>
             <ul className='my-7 max-w-2xl list-none'>
               {filteredBlogPosts.map(({ id, date, title, tags }) => (
                 <li className='border-slate-300 border-b-2 border-solid pb-8 mt-4 no-underline ml-0' key={id}>
                   <Link href={`/${id}`}>
-                    <h5 className='font-open-sans text-xl font-medium text-slate-900 hover:bg-sky-200 active:focus:bg-sky-400 pb-4 w-fit no-underline'>
+                    <h5 className='font-open-sans text-xl font-medium text-slate-900 hover:underline hover:bg-yellow-50 active:focus:bg-sky-400 pb-4 w-fit no-underline'>
                       {title}
                     </h5>
                   </Link>                  
                   <div className='flex flex-wrap'>
-                    <small className='pt-1 font-open-sans text-slate-600 hover:bg-sky-200 active:focus:bg-sky-400 w-fit no-underline pr-4'>
+                    <small className='pt-1 font-open-sans text-slate-600 hover:bg-yellow-50 active:focus:bg-sky-400 w-fit no-underline pr-4'>
                       <Date dateString={date} />
                     </small>
                     { tags.map(tag => (
-                      <button key={tag} onClick={() => setSearch(tag)} className="bg-sky-100 text-sky-800 text-sm font-open-sans font-semibold rounded-full px-3 py-1 mr-2 w-fit">
+                      <button key={tag} onClick={() => setSearch(tag)} className="bg-sky-100 hover:bg-green-100 text-sky-800 text-sm font-open-sans font-semibold rounded-full px-3 py-1 mr-2 w-fit">
                         {tag}
                       </button>
                     ))}
