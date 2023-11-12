5d4cbc3a03ac05e00cc460dde3238dedf6b821a8
paultreanor <treanorpaul9@gmail.com>
2023-11-12 20:30:06 +0000

    Fix padding for icons

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home({ allPagesData }) {
         <p className="text-lg font-open-sans">I'm a fullstack indie developer until I run out of money. This website is my digital notebook.</p>
         <p className="text-lg font-open-sans">Feel free to get in touch!</p>
 
-        <div className="row text-2xl pt-3j">
+        <div className="row text-2xl pt-3">
           <a 
             className="mr-3 pr-2 pt-4 rounded-full hover:bg-green-100 transition-colors duration-200" 
             href="https://github.com/PaulTreanor">
